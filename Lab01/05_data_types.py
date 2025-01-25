0c871e1d5e085321ddfe202cc688ff3575ffcd40
effuone <albkfil@gmail.com>
2025-01-25 19:06:20 +0500

    feat: lab1 done

---
+++ Lab01/05_data_types.py
@@ -0,0 +1,24 @@
+x = str("Hello World")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+python_datatypes = {
+    "str": "String",
+    "int": "Integer",
+    "float": "Float",
+    "complex": "Complex",
+    "list": "List",
+    "tuple": "Tuple",
+    "range": "Range",
+    "dict": "Dictionary",
+    "set": "Set",
+    "frozenset": "Frozen set",
+    "bool": "Boolean",
+    "bytes": "Bytes",
+    "bytearray": "Bytearray",
+    "memoryview": "Memoryview"
+}
